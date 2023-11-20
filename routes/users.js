af03d9fa58a85c3df8ad7cb859e73560f5a1792d
Abarnaa <abarna.balakrishanan@gmail.com>
2023-11-20 11:44:22 +0530

    CHANGES DONE

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friedns", verifyToken, getUserFriends);
 
-/* UPDATE API */
+/* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
