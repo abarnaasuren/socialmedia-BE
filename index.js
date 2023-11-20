adb2c358001e7dd5328a2c14f06b32d351adf0e1
Abarnaa <abarna.balakrishanan@gmail.com>
2023-11-20 11:43:30 +0530

    changes done

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname,'public/assets')));
 
-/* FILE STORAGE */
+/* FILE STORAGE  */
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null,"public/assets");
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friedns", verifyToken, getUserFriends);
 
-/* UPDATE */
+/* UPDATE API */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
